9e9a7221a24912eb733b1ea28c062964ec5076a0
tnnrcantcode <phamhoangbao1501@gmail.com>
2024-06-06 22:15:27 -0400

    bracket

--- UnoStartUp.java
+++ UnoStartUp.java
@@ -3,7 +3,8 @@
 public class UnoStartUp{
     //drgnjhgd
     //HI MY NAME IS AVANEESH
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         System.out.println("Hello");
         System.out.println("Avaneesh is slow in the head");
     }
