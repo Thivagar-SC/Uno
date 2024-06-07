7a87a393cd2d99b41d5c6ffb78b5ff50ddf01082
Avaneesh1234 <Avaneesh.Nitya@gmail.com>
2024-06-07 14:39:54 -0400

    finished card.java almost done player

--- Card.java
+++ Card.java
@@ -1,3 +1,21 @@
 public class Card {
+    private static int cardFaceValue;  //value of card
+private String color; //color of card
+
+public Card(int cardFaceValue, String color)
+{
+    this.cardFaceValue = cardFaceValue;
+    this.color = color;
+}
     
+public int getValue()
+{
+    return getValue();
+}
+     
+public String getColour()
+{
+    return getColour();
+}
+     
 }
