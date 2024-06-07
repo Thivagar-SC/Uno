d18a28c46346ee8b654cba26d0b951d3216c5f09
Thivagar-SC <StirCopter@gmail.com>
2024-06-07 15:02:14 -0400

    TEsnjgesnd

--- UnoStartUp.java
+++ UnoStartUp.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+
 import javax.swing.*;
 
 public class UnoStartUp{
@@ -5,17 +7,13 @@ public class UnoStartUp{
     //HI MY NAME IS AVANEESH
     public static void main(String[] args)
     {
-        System.out.println("Hello");
-        System.out.println("Tanner is slow in the head");
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setVisible(true);
-        frame.setTitle("Tanner sucks!");
-
-        JLabel label = new JLabel("Tanner is gay"); //typw 
-        frame.add(label);
+        UnoView a = new UnoView();
+        JFrame test = new JFrame();
+        test.setSize(new Dimension(1000,1000));
+        a.displayCards3();
+        test.setContentPane(a);
+        test.setVisible(true);
 
     }
 }
