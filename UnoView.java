1c796ff5b448a94b9d588fb3a94f4499388d8e8f
tnnrcantcode <phamhoangbao1501@gmail.com>
2024-06-07 13:31:48 -0400

    type shit

--- UnoView.java
+++ UnoView.java
@@ -1,8 +1,22 @@
 import javax.swing.*;
-import java.awt.event.KeyEvent;
+import java.awt.event.*;
+import java.awt.*;
 
 public class UnoView extends JPanel
 {
+    public static void main(String[] args) {
+        System.out.println("tba");
+        ImgComponent a = new ImgComponent("2156_uno_block.png");
+        JPanel b = new JPanel();
+        JFrame c = new JFrame();
+        b.setPreferredSize(new Dimension(200,200));
+        b.add(a);
+        c.setSize(new Dimension(500,900));
+        c.setContentPane(b);
+        c.setResizable(false);
+        c.setVisible(true);
+    }
+
     public void escInput (int keyCode)
     {
         if (keyCode == KeyEvent.VK_ESCAPE)
