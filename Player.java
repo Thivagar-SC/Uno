e68485d84fb97934b452996f5da5bd14f24455a1
Avaneesh1234 <Avaneesh.Nitya@gmail.com>
2024-06-07 13:25:50 -0400

    created methods

--- Player.java
+++ Player.java
@@ -1,3 +1,46 @@
+import java.util.ArrayList;
+
 public class Player {
+private String playerName; //name of the plyaer
+private boolean wonRound; //if player won or not
+private int totalScore; //score of player
+private int playerNumber; //value of player (determines order)
+private ArrayList<Card> cards;
+
+public Player(int playerNumber, String playerName)
+{
     
+}
+public void addCard(Card card, String source)
+{
+
+}
+public void placeCard(int cardIndex)
+{
+
+}
+public void setSelectable()
+{
+
+}
+public ArrayList<Card> getHand() 
+{
+    return cards;
+}
+public String getPlayerName()
+{
+    return playerName;
+}
+public void setWon()
+{
+
+}
+public int getTotalScore()
+{
+    return totalScore;
+}
+public void organizeDeck()
+{
+
+}
 }
--- UnoAi.java
+++ UnoAi.java
@@ -1,3 +1,31 @@
+import java.util.ArrayList;
+
 public class UnoAi {
+private double delayTime; // delays the time of the AI's actions
+private ArrayList<Card> cards; // list of cards ai has 
+private boolean wonRound;  //ai won the game
+private int playerNumber; //value for player order
+
+public UnoAi(int playerNumber)
+{
     
 }
+public void placeCard(int cardIndex)
+{
+
+}
+public void hitUNO()
+{
+
+}
+public void hitBlock()
+{
+
+}
+public void addCard( Card card, String source)
+{
+
+}
+
+}
+
