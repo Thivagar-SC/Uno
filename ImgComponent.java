40b61b15a0de300bb91a2b62484282013fdf077a
Thivagar-SC <StirCopter@gmail.com>
2024-06-09 15:27:59 -0400

    filestuff

--- ImgComponent.java
+++ ImgComponent.java
@@ -4,11 +4,17 @@ import java.io.*;;
 
 public class ImgComponent extends JComponent{
     ImageIcon image;
+    File holder;
+    JFileChooser a;
 
     public ImgComponent(String imageFile){
         super();
-        this.image = new ImageIcon(imageFile);
+        holder = new File(getClass().getResource("ImgHolder").getFile());
+        File[] test2 = holder.listFiles();
+        this.image = new ImageIcon(test2[0].getAbsolutePath());
         this.setPreferredSize(new Dimension(200,300));
+        //a = new JFileChooser("ImgHolder");
+        //System.out.println(image.getImage().);
 
         File test = new File(imageFile);
         if (!test.exists()){
