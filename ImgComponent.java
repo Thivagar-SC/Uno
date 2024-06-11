ee83505a6e525f25c44e6d377117bc6f060657d3
Thivagar-SC <StirCopter@gmail.com>
2024-06-11 14:29:06 -0400

    njdrfugdr

--- ImgComponent.java
+++ ImgComponent.java
@@ -1,9 +1,6 @@
 import javax.swing.*;
 import java.awt.*;
 import java.io.*;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class ImgComponent extends JComponent{
     ImageIcon image;
@@ -13,29 +10,13 @@ public class ImgComponent extends JComponent{
 
     public ImgComponent(String imageFile){
         super();
-        holder = new File(getClass().getResource("ImgHolder").getFile());
-        test2 = holder.listFiles();
 
-        System.out.println(holder+" THIS IS THES DRNGJNHRDUGH");
-        try {
-            this.image = new ImageIcon(test2[1].getCanonicalPath());
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
+        this.image = new ImageIcon(imageFile);
         this.setPreferredSize(new Dimension(200,300));
-        //a = new JFileChooser("ImgHolder");
-        //System.out.println(image.getImage().);
-
-        File test = new File(imageFile);
-        //if (!test.exists()){
-          //  System.err.println("ERROR FINDING IMAGE"); //temporary result
-        //}
     }
-
 
     public void paintComponent(Graphics g){
         super.paintComponent(g);
-        Image pic = this.image.getImage();
-        g.drawImage(pic, 0, 0,200,300, null);
+        g.drawImage(this.image.getImage(),0, 0,200,300, null);
     }
 }
