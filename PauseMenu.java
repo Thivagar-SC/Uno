bb355b111a5663f947045e8a0dc63a4709c1774a
tnnrcantcode <phamhoangbao1501@gmail.com>
2024-06-14 14:08:48 -0400

    Add files via upload

--- PauseMenu.java
+++ PauseMenu.java
@@ -18,7 +18,6 @@ public class PauseMenu extends JPanel {
     public JButton resumeButton; // Interactable buttons
     public JButton quitToMainMenuButton;
     public JButton quitGameButton;
-    public JPanel buttonsBackground;
 
     /**
      * pauseMenu
@@ -30,7 +29,7 @@ public class PauseMenu extends JPanel {
         super();
         isPaused = false;
         this.setVisible(this.isPaused);
-        this.setLayout(null);
+        this.setLayout(new FlowLayout());
 
         // setting text
         resumeButton = new JButton("Resume");
@@ -48,34 +47,25 @@ public class PauseMenu extends JPanel {
         quitGameButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         // Background for pause menu
-        buttonsBackground = new JPanel();
+        JPanel buttonsBackground = new JPanel();
         this.add(buttonsBackground);
         buttonsBackground.setPreferredSize(new Dimension(300,180));
         buttonsBackground.setLayout(new BoxLayout(buttonsBackground, BoxLayout.Y_AXIS));
-        this.buttonsBackground.setBounds(0, 100, 300, 180);
 
         // OUTPUT
-        //buttonsBackground.add(Box.createVerticalStrut(40));
+        buttonsBackground.add(Box.createVerticalStrut(40));
         buttonsBackground.add(resumeButton);
-        //buttonsBackground.add(Box.createVerticalStrut(10));
+        buttonsBackground.add(Box.createVerticalStrut(10));
         buttonsBackground.add(quitToMainMenuButton);
-        //buttonsBackground.add(Box.createVerticalStrut(10));
+        buttonsBackground.add(Box.createVerticalStrut(10));
         buttonsBackground.add(quitGameButton);
-        //buttonsBackground.add(Box.createVerticalStrut(20));
+        buttonsBackground.add(Box.createVerticalStrut(20));
 
     }
 
     public void setVisibility(){
         this.isPaused = !this.isPaused;
         this.setVisible(this.isPaused);
-    }
-
-
-    public void update() {
-    }
-
-    public boolean isPaused(){
-        return this.isPaused;
     }
 
     @Override
